f844f6e1fb3ba8fa8350789073aac121835d30ea
lintool <jimmylin@uwaterloo.ca>
2016-10-10 12:46:52 -0400

    Changed to use primitive iterators to avoid boxing/unboxing.

--- graphjet-core/src/main/java/com/twitter/graphjet/algorithms/PageRank.java
+++ graphjet-core/src/main/java/com/twitter/graphjet/algorithms/PageRank.java
@@ -3,24 +3,25 @@ package com.twitter.graphjet.algorithms;
 import com.google.common.util.concurrent.AtomicDouble;
 import com.twitter.graphjet.bipartite.api.EdgeIterator;
 import com.twitter.graphjet.directed.api.OutIndexedDirectedGraph;
+import it.unimi.dsi.fastutil.longs.LongArrayList;
+import it.unimi.dsi.fastutil.longs.LongIterator;
 import it.unimi.dsi.fastutil.longs.LongOpenHashSet;
 
-import java.util.ArrayList;
-
 public class PageRank {
-  final private OutIndexedDirectedGraph bigraph;
+  final private OutIndexedDirectedGraph graph;
   final private LongOpenHashSet vertices;
-  final private long max;
+  final private long maxVertexId;
   final private double dampingFactor;
-  final private int nodeCount;
+  final private int vertexCount;
   final private double tolerance;
 
-  public PageRank(OutIndexedDirectedGraph bigraph, LongOpenHashSet vertices, long max, double dampingFactor, double tolerance) {
-    this.bigraph = bigraph;
+  public PageRank(OutIndexedDirectedGraph graph, LongOpenHashSet vertices,
+                  long maxVertexId, double dampingFactor, double tolerance) {
+    this.graph = graph;
     this.vertices = vertices;
-    this.max = max;
+    this.maxVertexId = maxVertexId;
     this.dampingFactor = dampingFactor;
-    this.nodeCount = vertices.size();
+    this.vertexCount = vertices.size();
     this.tolerance = tolerance;
   }
 
@@ -32,41 +33,49 @@ public class PageRank {
     return ret;
   }
 
-  public double[] iterate(double[] prevPR, double dampingAmount, ArrayList<Long> noOuts, AtomicDouble error) {
-    double afterPR[] = new double[(int) (max + 1)];
+  public double[] iterate(double[] prevPR, double dampingAmount, LongArrayList noOuts, AtomicDouble error) {
+    double afterPR[] = new double[(int) (maxVertexId + 1)];
     AtomicDouble dangleSum = new AtomicDouble();
-    noOuts.forEach(v -> dangleSum.addAndGet(prevPR[(int) (long) v]));
-    dangleSum.set(dangleSum.get() / nodeCount);
+    LongIterator iter = noOuts.iterator();
+    while (iter.hasNext()) {
+      dangleSum.addAndGet(prevPR[(int) iter.nextLong()]);
+    }
+    dangleSum.set(dangleSum.get() / vertexCount);
 
-    vertices.forEach(v -> {
-      int outDegree = bigraph.getOutDegree(v);
-      double outWeight = dampingFactor * prevPR[(int) (long) v] / outDegree;
-      EdgeIterator edges = bigraph.getOutEdges(v);
+    iter = vertices.iterator();
+    while (iter.hasNext()) {
+      long v = iter.nextLong();
+      int outDegree = graph.getOutDegree(v);
+      double outWeight = dampingFactor * prevPR[(int) v] / outDegree;
+      EdgeIterator edges = graph.getOutEdges(v);
       while (edges.hasNext()) {
         int nbr = (int) edges.nextLong();
         afterPR[nbr] += outWeight;
       }
-      afterPR[(int) (long) v] += dampingAmount + dangleSum.get();
-    });
+      afterPR[(int) v] += dampingAmount + dangleSum.get();
+    }
+
     error.set(deltaOfArrays(prevPR, afterPR));
     return afterPR;
   }
 
   public double[] initializePR(int nodeCount) {
-    double prevPR[] = new double[(int) (max + 1)];
+    double prevPR[] = new double[(int) (maxVertexId + 1)];
     vertices.forEach(v -> prevPR[(int) (long) v] = 1.0 / nodeCount);
     return prevPR;
   }
 
   public double[] run(int maxIteration) {
-    ArrayList<Long> noOuts = new ArrayList<>();
-    vertices.forEach(v -> {
-      if (bigraph.getOutDegree(v) == 0) {
+    LongArrayList noOuts = new LongArrayList();
+    LongIterator iter = noOuts.iterator();
+    while (iter.hasNext()) {
+      long v = iter.nextLong();
+      if (graph.getOutDegree(v) == 0) {
         noOuts.add(v);
       }
-    });
-    double dampingAmount = (1.0 - dampingFactor) / nodeCount;
-    double prevPR[] = initializePR(nodeCount);
+    }
+    double dampingAmount = (1.0 - dampingFactor) / vertexCount;
+    double prevPR[] = initializePR(vertexCount);
 
     int i = 0;
     AtomicDouble error = new AtomicDouble(Double.MAX_VALUE);
@@ -74,7 +83,7 @@ public class PageRank {
       prevPR = iterate(prevPR, dampingAmount, noOuts, error);
       i++;
     }
-    System.out.println("# of iterations " + i);
+
     return prevPR;
   }
 }
